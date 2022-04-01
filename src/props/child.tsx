57ca47a2cce1a37955a1645ff76f9c0fde4907f7
pavke987 <pavke987@gmail.com>
2022-04-01 22:23:52 +0200

    typescript

---
+++ src/props/child.tsx
@@ -0,0 +1,13 @@
+
+interface ChildProps {
+  color: string;
+};
+
+
+export const Child = ({ color }: ChildProps) => {
+  return <div>{color}</div>
+};
+
+export const ChildAsFC: React.FC<ChildProps> = ({ color }) => {
+  return <div>{ color }</div>
+} 
